e208a6e39294a9dc62b2282a2061cfa5fb64b76a
ilikebeans676767 <roanfyne@hotmail.com>
2025-10-28 18:52:54 -0700

    Update game.js

--- js/game.js
+++ js/game.js
@@ -40,7 +40,7 @@ var PIPE_COUNT = 6; // Start with 6 pipes for higher difficulty
 var PIPE_GAP = 90;
 var PIPE_WIDTH = 25, PIPE_HEIGHT = 135; // Original pipe dimensions
 var PIPE_SPAWN_DISTANCE = Math.floor(canvas.width / PIPE_COUNT);
-var INITIAL_PIPE_SPEED = -60;
+var INITIAL_PIPE_SPEED = -48;
 var pipeSpeed = INITIAL_PIPE_SPEED;
 var pipesPassed = 0;
 
@@ -164,7 +164,7 @@ var update = function (modifier)
 			pipesPassed++;
 			uppers[i].counted = true;
 			// Every 6 pipes passed, increase speed
-			if (pipesPassed % 6 === 0) {
+			if (pipesPassed % 4 === 0) {
 				pipeSpeed -= 12; // Increase speed by 5 (more negative is faster)
 			}
 		}
